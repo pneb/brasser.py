377a5137301e488c2e4eb31c83d214edbf002b19
Bernward Sanchez <sanzbernward@gmail.com>
2022-12-27 19:49:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='brasser',
-    version='1.0.2',
+    version='1.0.3',
     author='Bernward Sanchez',
     author_email='contact@bern.codes',
     description='A library for generating cryptographically random UUIDs',
